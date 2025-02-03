610c4f48769d201b42c23e556869104e4c7c6549
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-02-03 13:58:58 -0500

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,6 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
 
-router.get('/api/csrf/restore', (_req, _res) => {
-    if (process.env.NODE_ENV !== 'production') {
-        router.get('/api/csrf/restore', (req, res) => {
-            res.cookie('XSRF-TOKEN', req.csrfToken());
-            return res.json({});
-        });
-    }
-});
+router.use(restoreUser);
 
 module.exports = router;
